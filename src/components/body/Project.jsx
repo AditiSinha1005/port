9e7a72773b2b5c06c63af7e0eddf73e25f30b18c
AditiSinha1005 <swati.sinha.412@gmail.com>
2022-07-30 01:09:05 +0530

    done

--- src/components/body/Project.jsx
+++ src/components/body/Project.jsx
@@ -2,46 +2,41 @@ import React from 'react'
 import ProjectCard from './ProjectCard'
 import ashes from "../../assets/Studying-bro.png";
 import hulu from "../../assets/hulu.png";
+import instantmoviefinder from  "../../assets/instantmoviefinder.png"
 import Separator from './Separator';
 
-const Project = () => {
+function Project() {
   return (
     <>
-      <Separator/>
-      <div style={{marginTop: "1rem", fontSize:"2rem"}}>Project</div>
+      <Separator />
+      <div style={{ marginTop: "1rem", fontSize: "2rem" }}>Project</div>
       <div className="projectCardBox">
         <span>
           <ProjectCard
             title="Hulu"
-            description="It's an app that lets you find the movies of your choice made in react js"
+            description="It's an app similar to netflix that shows you the best shows,movies, webseries etc "
             link="https://port-umber-tau.vercel.app/"
-            image={hulu}
-          />
+            image={hulu} />
         </span>
         <span>
+          <ProjectCard
+            title="Instant movie finder"
+            description="It's instantly helps you search your favourite movies and info about them"
+            image={instantmoviefinder} />
+        </span>
+        <span>
+          <ProjectCard
+            title="Google keep clone"
+            description="It's basically a clone of Googlekeep that lets you add tasks to your list to keep track of the day"
+            image={ashes} />
+        </span>
+        {/* <span>
           <ProjectCard
             title="HumbaBumba"
             description="adlsjfl fjalkdsfj laksdjf lkasdfj klasdfj kl;asdjf l;kasdjf l;kasdjf kl;asdfj lkasdjf akl;sdfj l;aksd"
             link="instagram.com"
-            image={ashes}
-          />
-        </span>
-        <span>
-          <ProjectCard
-            title="HumbaBumba"
-            description="adlsjfl fjalkdsfj laksdjf lkasdfj klasdfj kl;asdjf l;kasdjf l;kasdjf kl;asdfj lkasdjf akl;sdfj l;aksd"
-            link="instagram.com"
-            image={ashes}
-          />
-        </span>
-        <span>
-          <ProjectCard
-            title="HumbaBumba"
-            description="adlsjfl fjalkdsfj laksdjf lkasdfj klasdfj kl;asdjf l;kasdjf l;kasdjf kl;asdfj lkasdjf akl;sdfj l;aksd"
-            link="instagram.com"
-            image={ashes}
-          />
-        </span>
+            image={ashes} />
+        </span> */}
       </div>
     </>
   );
